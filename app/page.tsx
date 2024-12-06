551839032b95e81a725ae1ddc443161475e55f81
AshkiX <arashk.knj@gmail.com>
2024-12-06 15:14:00 +0300

    Fix full-width issue

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,18 @@ export default function Home() {
     <>
       <Toaster />
 
-      <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
+      <section className="min-h-screen w-full grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
           <Image
             src="/mobile-app.png"
             alt="Mobile App Screenshots"
             fill
+            priority
             className="object-contain mt-8 md:mt-24 px-14 object-bottom"
           />
         </div>
 
-        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
+        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10 md:pr-12">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
             Join the conversation: Secure your spot on our Chat App!
           </h1>
